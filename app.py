0e52dfb1553f79830b2516ac1ef651cad79ef565
I-reall <97663203+I-reall@users.noreply.github.com>
2024-05-04 11:23:20 +0000

    Added a background picture and style

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    bg_image_url = url_for('static', filename='images/DALL·E 2024-05-04 13.09.36 - A beautiful and vibrant photo of an assortment of decorative cakes on display at a bakery. The image features a variety of cakes with different design.webp')
+    return render_template('home.html', bg_image_url=bg_image_url)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
